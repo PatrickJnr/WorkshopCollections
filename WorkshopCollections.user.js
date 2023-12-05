835440d60b4855c66951443022dd9f08769e5224
Patrick Jr <patrickcaulton423@gmail.com>
2023-12-05 21:43:39 +0000

    Update WorkshopCollections.user.js
    ## Change Log
    
    1. **Refactored Button Creation:**
       - Changed button creation from `var btn_add = document.createElement("BUTTON");` to `const btnAdd = createButton('ASCM_addall', 'Add All', '120px');`.
    
    2. **Button Placement:**
       - Adjusted button placement to be directly under the ".collectionAddItemsSection" div.
    
    3. **Button Styling:**
       - Removed inline styling for buttons and applied styles within the `createButton` function.
    
    4. **Button Event Listeners:**
       - Changed event listeners from jQuery syntax (`jQuery('button#ASCM_addall').click(...)`) to vanilla JavaScript (`btnAdd.addEventListener('click', ...)`).
    
    5. **Button Styling (Background Color and Border):**
       - Updated button background color and removed borders for a cleaner appearance.
    
    6. **Button Container Styling:**
       - Adjusted styling of the container div for the buttons.
    
    7. **Positioning of Buttons:**
       - Moved the positioning of the buttons into the `createButton` function.
    
    These changes aim to enhance code structure, readability, and maintainability. If you have any specific questions about the modifications, feel free to ask!

--- WorkshopCollections.user.js
+++ WorkshopCollections.user.js
@@ -1,83 +1,98 @@
 // ==UserScript==
 // @name         Workshop Collections Mass Add
 // @namespace    http://social.grimtech.co.uk
-// @version      0.4
+// @version      1.0
 // @description  A quick and simple script to mass add your subscribed items into a collection
 // @author       PatrickJr / kluvo2
 // @match        https://steamcommunity.com/sharedfiles/managecollection*
-// @icon         https://steamworkshopdownloader.io/android-icon-192x192.png
+// @icon         https://steamcommunity.com/favicon.ico
 // @downloadURL  https://raw.githubusercontent.com/PatrickJnr/WorkshopCollections/main/WorkshopCollections.user.js
 // @updateURL    https://raw.githubusercontent.com/PatrickJnr/WorkshopCollections/main/WorkshopCollections.user.js
 // @grant        none
 // ==/UserScript==
 
-setTimeout(function(){
-    // Create "Add" button
-    var btn_add = document.createElement("BUTTON");
-    var collection_window = document.querySelector('div.collectionAddItemsSection')
-    collection_window.insertBefore(btn_add,collection_window.firstChild);
-    btn_add.setAttribute('id','ASCM_addall');
-    jQuery('button#ASCM_addall').html('Add All')
-    btn_add.style.position = 'absolute';
-    btn_add.style.top = '116px';
-    btn_add.style.right = '120px';
-    btn_add.border = 'linear-gradient( to bottom, rgba(47,137,188,1) 5%, rgba(23,67,92,1) 95%)';
-    btn_add.style['border-radius'] = '2px';
-    btn_add.style.color = '#A4D7F5';
-    btn_add.style['font-size'] = 'inherit';
-    btn_add.style.background = 'linear-gradient( to bottom, rgba(47,137,188,1) 5%, rgba(23,67,92,1) 95%)';
-    btn_add.style.width = 'auto';
-    btn_add.style.height = 'auto';
-    btn_add.style['text-decoration'] = 'none';
-    // Create "Remove" button
-    var btn_rem = document.createElement("BUTTON");
-    var collection_window = document.querySelector('div.collectionAddItemsSection')
-    collection_window.insertBefore(btn_rem ,collection_window.firstChild);
-    btn_rem .setAttribute('id','ASCM_removeall');
-    jQuery('button#ASCM_removeall').html('Remove All')
-    btn_rem.style.position = 'absolute';
-    btn_rem.style.top = '116px';
-    btn_rem.style.right = '35px';
-    btn_rem.style['border-radius'] = '2px';
-    btn_rem.style.color = '#A4D7F5';
-    btn_rem.style['font-size'] = 'inherit';
-    btn_rem.style.background = 'linear-gradient( to bottom, rgba(47,137,188,1) 5%, rgba(23,67,92,1) 95%)';
-    btn_rem.style.width = 'auto';
-    btn_rem.style.height = 'auto';
-    btn_rem.style['text-decoration'] = 'none';
-    // Bind "Add" button
-    jQuery('button#ASCM_addall').click(function(){
-        var items = [];
-        var collection_name = jQuery('div.manageCollectionHeader div.breadcrumbs a').eq(2).text().trim();
-        var url = new URL(document.location.href);
-        var collection_id = url.searchParams.get('id');
-        jQuery('div#MySubscribedItems div.itemChoice:not(.inCollection)').each(function(){
-            var data = {
-                id: collection_id,
-                sessionid: window.g_sessionID,
-                childid: jQuery(this).attr('id').replace('choice_MySubscribedItems_',''),
-                activeSection: collection_name
-            };
-            addToCollection(data, jQuery(this));
+(function () {
+    'use strict';
+
+    setTimeout(function () {
+        const collectionWindow = document.querySelector('div.collectionAddItemsSection');
+
+        // Create a container div for buttons
+        const buttonsContainer = document.createElement('div');
+        buttonsContainer.style.position = 'relative';
+        buttonsContainer.style.marginTop = '10px'; // Adjust the vertical position as needed
+        collectionWindow.appendChild(buttonsContainer);
+
+        // Create "Add" button
+        const btnAdd = createButton('ASCM_addall', 'Add All', '120px');
+        buttonsContainer.appendChild(btnAdd);
+
+        // Create "Remove" button
+        const btnRemove = createButton('ASCM_removeall', 'Remove All', '35px');
+        buttonsContainer.appendChild(btnRemove);
+
+        // Style the buttons
+        btnAdd.style.backgroundColor = '#5cb85c';
+        btnAdd.style.border = 'none'; // Remove border
+
+        btnRemove.style.backgroundColor = '#d9534f';
+        btnRemove.style.border = 'none'; // Remove border
+
+        // Bind "Add" button
+        btnAdd.addEventListener('click', function () {
+            const items = [];
+            const collectionName = jQuery('div.manageCollectionHeader div.breadcrumbs a').eq(2).text().trim();
+            const url = new URL(document.location.href);
+            const collectionId = url.searchParams.get('id');
+
+            jQuery('div#MySubscribedItems div.itemChoice:not(.inCollection)').each(function () {
+                const data = {
+                    id: collectionId,
+                    sessionid: window.g_sessionID,
+                    childid: jQuery(this).attr('id').replace('choice_MySubscribedItems_', ''),
+                    activeSection: collectionName
+                };
+                addToCollection(data, jQuery(this));
+            });
         });
-    });
-    // Bind "Remove" button
-    jQuery('button#ASCM_removeall').click(function(){
-        jQuery('div#MySubscribedItems div.itemChoice.inCollection').each(function(){
-            window.RemoveChildFromCollection(jQuery(this).attr('id').replace('choice_MySubscribedItems_',''))
+
+        // Bind "Remove" button
+        btnRemove.addEventListener('click', function () {
+            jQuery('div#MySubscribedItems div.itemChoice.inCollection').each(function () {
+                window.RemoveChildFromCollection(jQuery(this).attr('id').replace('choice_MySubscribedItems_', ''));
+            });
         });
-    });
-    // Function to send a request to add item to a collection
-    function addToCollection(data, object){
-        jQuery.ajax({
-            type: "POST",
-            url: 'https://steamcommunity.com/sharedfiles/addchild',
-            data: data,
-            success: function(response){
-                if(object && response.success == 1){
-                    object.addClass('inCollection');
+
+        // Function to create a button
+        function createButton(id, text, rightPosition) {
+            const button = document.createElement('button');
+            button.setAttribute('id', id);
+            button.innerHTML = text;
+            button.style.borderRadius = '2px';
+            button.style.color = '#fff';
+            button.style.fontSize = 'inherit';
+            button.style.background = 'linear-gradient( to bottom, rgba(47,137,188,1) 5%, rgba(23,67,92,1) 95%)';
+            button.style.width = 'auto';
+            button.style.height = 'auto';
+            button.style.textDecoration = 'none';
+            button.style.padding = '5px 10px'; // Added padding for better visibility
+            button.style.marginRight = '10px'; // Added margin for spacing between buttons
+
+            return button;
+        }
+
+        // Function to send a request to add an item to a collection
+        function addToCollection(data, object) {
+            jQuery.ajax({
+                type: 'POST',
+                url: 'https://steamcommunity.com/sharedfiles/addchild',
+                data: data,
+                success: function (response) {
+                    if (object && response.success == 1) {
+                        object.addClass('inCollection');
+                    }
                 }
-            }
-        });
-    }
-}, 0);
+            });
+        }
+    }, 0);
+})();
